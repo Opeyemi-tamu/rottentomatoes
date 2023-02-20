38a6338caf2fb2a69f9cb16c41046e5ae810c027
0x0384 <0x0384@gmail.com>
2023-02-20 07:49:19 -0600

    added new movies to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@ more_movies = [
     {:title => 'Green Book', :rating => 'PG-13', :release_date => '16-Nov-2018'},
     {:title => 'Parasite', :rating => 'R', :release_date => '30-May-2019'},
     {:title => 'Nomadland', :rating => 'R', :release_date => '19-Feb-2021'},
-    {:title => 'CODA', :rating => 'PG-13', :release_date => '13-Aug-2021'}
+    {:title => 'CODA', :rating => 'PG-13', :release_date => '13-Aug-2021'},
+    {:title => 'Game of Thrones', :rating => 'PG-18', :release_date => '11-Apr-2011'},
+    {:title => 'Merlin', :rating => 'PG-14', :release_date => '20-Sep-2008'}
 ]
 
 
